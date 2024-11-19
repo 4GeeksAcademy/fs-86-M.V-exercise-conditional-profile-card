88507b54d837291942a8283845143c779050170f
M.V <marco.mov98@gmail.com>
2024-11-19 10:18:18 +0000

    First Commit

--- src/js/app.js
+++ src/js/app.js
@@ -33,10 +33,12 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
+          <h1>${variables.name || "Jenny"} ${variables.lastName ||
+    "Ortega"}</h1>
+          <h2>${variables.role || "Full Stack Developer"}</h2>
+          <h3>${variables.country || "Spain"} ${variables.city ||
+    "Valencia"}</h3>
+          <ul class="${variables.socialMediaPosition}">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
@@ -54,9 +56,11 @@ window.onload = function() {
     // if includeCover is true the algorithm should show the cover image
     includeCover: true,
     // this is the image's url that will be used as a background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    background:
+      "https://imgcdn.stablediffusionweb.com/2024/9/11/20a4982b-cc34-4e80-9762-b4979c39829f.jpg",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1TVHTjn91L3MTf5wGyNSa9elWVPwQjCMC8w&s",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
