889728a55fb43dd6cbf54c3a59d84f2d583c5e04
M.V <marco.mov98@gmail.com>
2024-11-24 00:33:29 +0000

    ya no quiero hacer mas commits en este proyecto pls

--- src/js/app.js
+++ src/js/app.js
@@ -41,10 +41,18 @@ function render(variables = {}) {
     "Marco-MOV"} ${variables.linkedin || "LinkedIn.com"}</h3>
 
           <ul class="${variables.socialMediaPosition}">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://twitter.com/${
+              variables.twitter
+            }"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/Marco-MOV/${
+              variables.github
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${
+              variables.linkedin
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/_just.me.marco/${
+              variables.instagram
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
